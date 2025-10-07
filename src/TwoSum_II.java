74b1aaaf6f5ac6bb1c5a6bdb6a86e7ffdc23b206
Divya Reddy <divya.nagavelly@gmail.com>
2025-10-07 16:08:58 +0530

    day 3 : 27,283,167 : count -9

---
+++ src/TwoSum_II.java
@@ -0,0 +1,17 @@
+public class TwoSum_II {
+    public int[] twoSum(int[] numbers, int target) {
+        int start=0;
+        int n=numbers.length;
+        int end=n-1;
+
+        while(start<end){
+            if(numbers[start]+numbers[end]==target){
+                return new int[]{start+1,end+1};
+            }
+            else if(numbers[start]+numbers[end]<target) start++;
+            else end--;
+        }
+
+        return new int[]{-1,-1};
+    }
+}
